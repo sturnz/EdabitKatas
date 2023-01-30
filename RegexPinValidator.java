aa9b53a8b037a3b296d1d72e751e9f0a2b3a91aa
Micaeli <109481776+sturnz@users.noreply.github.com>
2023-01-30 22:51:56 +0100

    Added solution
    Added solution for "RegEX:ValidatePIN"

---
+++ RegexPinValidator.java
@@ -0,0 +1,25 @@
+// Website with full Kata: https://edabit.com/challenge/sCP58J9vcexchgwo7
+// Create a function that will test if a string is a valid PIN or not via a regular expression.
+//
+//    A valid PIN has:
+//
+//    Exactly 4 or 6 characters.
+//    Only numeric characters (0-9).
+//    No whitespace.
+
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RegexPinValidator {
+    public boolean validate(String pin){
+        // minimum of digits:  4
+        // maximum of digits: 10
+        String  regex   = "[0-9]{4,6}";
+
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(pin);
+
+        return matcher.matches();
+    }
+}
